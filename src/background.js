80f764a8a4e768256e5d4d06404433d123cef4d4
podhmo <ababjam61+github@gmail.com>
2023-04-23 01:25:11 +0900

    chore: restructure

--- src/background.js
+++ src/background.js
@@ -1,3 +1,5 @@
+import { flashBadge } from "./lib/badge.js"
+
 const offscreenDocumentPath = "offscreen.html";
 
 
@@ -99,47 +101,3 @@ async function hasOffscreenDocument(path) {
 // async function addToClipboardV2(value) {
 //     navigator.clipboard.writeText(value);
 //   }
-
-
-// ----------------------------------------
-// see: https://github.com/yorkxin/copy-as-markdown
-const COLOR_GREEN = '#738a05';
-const COLOR_RED = '#d11b24';
-const COLOR_OPAQUE = [0, 0, 0, 255];
-
-const TEXT_OK = '✓';
-const TEXT_ERROR = '×';
-const TEXT_EMPTY = '';
-
-const FLASH_BADGE_TIMEOUT = 3000; // ms
-
-async function flashBadge(type) {
-  const entrypoint = chrome.action /* MV3 */ || chrome.browserAction; /* Firefox MV2 */
-
-  switch (type) {
-    case 'success':
-      await entrypoint.setBadgeText({ text: TEXT_OK });
-      await entrypoint.setBadgeBackgroundColor({ color: COLOR_GREEN });
-      break;
-    case 'fail':
-      await entrypoint.setBadgeText({ text: TEXT_ERROR });
-      await entrypoint.setBadgeBackgroundColor({ color: COLOR_RED });
-      break;
-    default:
-      return; // don't know what it is. quit.
-  }
-
-  chrome.alarms.create('clear', { when: Date.now() + FLASH_BADGE_TIMEOUT });
-}
-
-chrome.alarms.onAlarm.addListener((alarm) => {
-    const entrypoint = chrome.action /* MV3 */ || chrome.browserAction; /* Firefox MV2 */
-  
-    if (alarm.name === 'clear') {
-      Promise.all([
-        entrypoint.setBadgeText({ text: TEXT_EMPTY }),
-        entrypoint.setBadgeBackgroundColor({ color: COLOR_OPAQUE }),
-      ])
-        .then(() => { /* NOP */ });
-    }
-  });
